ff60aa24405d6c4584667fc44afbc00ec9b6a7dc
lijunnew <lijunnew@qq.com>
2021-06-07 01:01:59 +0800

    fix

---
+++ proxy.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    '/api/rdb': 'http://47.98.35.68',
+    '/api/ams-ce': 'http://47.98.35.68',
+    '/api/job': 'http://47.98.35.68',
+    '/api/ticket': 'http://47.98.35.68',
+    '/api/mon': 'http://47.98.35.68',
+    '/api/transfer': 'http://47.98.35.68',
+    '/api/index': 'http://47.98.35.68',
+  }
